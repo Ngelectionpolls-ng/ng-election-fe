3459d0347a00a2003b306e7f090ad3f732c2fe93
Kingsley <kingsleyakwa@gmail.com>
2024-09-22 16:13:47 +0100

    Feat: make sidebar responsive

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -9,13 +9,14 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+    const [showSidebar, setShowSidebar] = useState<boolean>(false);
     return (
-        <main className="w-full">
-            <DashboardNavBar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-            <section className={`${isCollapsed ? "pl-[80px]" : "pl-[230px]"} w-full pt-[100px] transition-all duration-300`}>
+        <main className="w-full transition-all duration-300">
+            <DashboardNavBar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} setShowSidebar={setShowSidebar} />
+            <section className={`${isCollapsed ? "md:pl-[80px]" : "md:pl-[230px]"} w-full pt-[100px] transition-all duration-300`}>
                 {children}
             </section>
-            <SideBar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+            <SideBar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} setShowSidebar={setShowSidebar} showSidebar={showSidebar} />
         </main>
     );
 }
